085082361b7396b5998157b273a9c06e75e862a5
Nick Sette <njs2394@gmail.com>
2023-11-02 11:30:25 -0400

    changes

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 const bookStore = {
   location: "Seattle",
-  address:'333 st ne Seattle wa 99999',
+  address: '333 st ne Seattle wa 99999',
   number: 9999999999,
   name: 'Easley\'s Technical Books',
   hours: 'Monday - Friday 9am - 6pm',
@@ -10,7 +10,7 @@ const bookStore = {
       title: 'Eloquent JavaScript: A Modern Introduction to Programming',
       author: 'Marjin Haverbeke',
       price: 10.00,
-      reviews: [{userID: 1, content:'Good book, but not great for new coders'}],
+      reviews: [{ userID: 1, content: 'Good book, but not great for new coders' }],
       inventory: 10,
       imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg'
     },
@@ -19,7 +19,7 @@ const bookStore = {
       title: 'JavaScript & JQuery: Interactive Front-End Web Development',
       author: 'Jon Duckett',
       price: 45.75,
-      reviews: [{userID: 15, content:'good way to learn JQuery'}],
+      reviews: [{ userID: 15, content: 'good way to learn JQuery' }],
       inventory: 2,
       imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg'
     },
@@ -28,7 +28,7 @@ const bookStore = {
       title: 'JavaScript: The Good Parts',
       author: 'Douglas Crockford',
       price: 36.00,
-      reviews: [{userID: 25, content:'I disagree with everything in this book'}, {userID: 250, content:'Only JS book anyone needs'}],
+      reviews: [{ userID: 25, content: 'I disagree with everything in this book' }, { userID: 250, content: 'Only JS book anyone needs' }],
       inventory: 8,
       imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
     },
@@ -37,7 +37,7 @@ const bookStore = {
       title: 'JavaScript: The Definitive Guide',
       author: 'David Flanagan',
       price: 25.50,
-      reviews: [{userID: 44, content:'Great intro to js book'}, {userID: 350, content:'It really is the Definitive guide'}],
+      reviews: [{ userID: 44, content: 'Great intro to js book' }, { userID: 350, content: 'It really is the Definitive guide' }],
       inventory: 0,
       imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg"
     },
@@ -46,16 +46,16 @@ const bookStore = {
       title: 'You Don’t Know JS',
       author: 'Kyle Simpson',
       price: 6.00,
-      reviews: [{userID: 76, content:'You can find this for free online, no need to pay for it!'}],
+      reviews: [{ userID: 76, content: 'You can find this for free online, no need to pay for it!' }],
       inventory: 7,
       imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg'
-    }, 
+    },
     {
       id: 6,
       title: 'Learn Enough JavaScript to Be Dangerous',
       author: 'Michael Hartl',
       price: 24.00,
-      reviews: [{userID: 50, content:'pretty good'}],
+      reviews: [{ userID: 50, content: 'pretty good' }],
       inventory: 5,
       imageUrl: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQyf6xSyTHc7a8mx17ikh6GeTohc88Hn0UgkN-RNF-h4iOwVlkW'
     },
@@ -64,9 +64,49 @@ const bookStore = {
       title: 'Cracking the Coding Interview',
       author: 'Gayle Laakmann McDowell',
       price: 49.95,
-      reviews: [{userID: 99, content:'One of the most helpful books for taking on the tech interview'}, {userID: 20, content: 'Great but I just wish it was in JavaScript instead of Java' }],
+      reviews: [{ userID: 99, content: 'One of the most helpful books for taking on the tech interview' }, { userID: 20, content: 'Great but I just wish it was in JavaScript instead of Java' }],
       inventory: 20,
       imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg'
     }
   ]
 }
+
+
+//Assingment from Rachel
+// Isolate John Duckett
+// Isolate image url from Marjin Haverbeke
+// Take inventory, create a new array of just the titles 
+
+// Isolate John Duckett
+
+
+//No Function 
+const findAuthor = 'Jon Duckett'
+let Author = bookStore.inventory.filter((el) => {
+  if (el.author == findAuthor) {
+    return el
+  }
+}
+)
+console.log(Author[0].author)
+
+//Through Function 
+function authorFinder(author) {
+  let filteringObjects = bookStore.inventory.filter((el) => {
+    if (el.author == author) {
+      return el
+    }
+  })
+
+  return (filteringObjects[0].author)
+
+}
+console.log(authorFinder(findAuthor))
+
+
+//Get Titles 
+let titles = bookStore.inventory.map((el) => {
+  return (el.title)
+})
+
+console.log(titles)
--- src/index.js
+++ src/index.js
@@ -6,13 +6,52 @@ function formatPrice(price) {
   return '$' + Number.parseFloat(price).toFixed(2);
 }
 
-//✅ 3. explore the debugger
+// ✅ 3. explore the debugger
 
 //✅ 4. update the store name with "easley's technical books"
 
+function renderHeader() {
+  let storeName = bookStore.name// 1. Get the info we need
+  let storeNameNode = document.querySelector('#store-name') // 2. Get the node we need
+  //Will return <h1 id="store-name" /h1>
+  storeNameNode.textContent = storeName //update the node
+}
+renderHeader()
+
+
 //✅ 5. create a function for render footer
 
+function renderFooter() {
+  let storeName = bookStore.name
+  let storeNumber = bookStore.number
+  let storeAddress = bookStore.address
+
+  let storeNameNode = document.querySelector('#store')
+  let storeNumberNode = document.querySelector('#number')
+  let storeAddressNode = document.querySelector('#address')
+
+  storeNameNode.textContent = storeName
+  storeNumberNode.textContent = storeNumber
+  storeAddressNode.textContent = storeAddress
+
+
+}
+renderFooter()
+
+
+
 //✅ 6. create a function called renderBook(book)
+
+
+/*
+<li class="card">
+  <h3>Eloquent JavaScript : A Modern Introduction to Programming</h3>
+  <p>Marjin Haverbeke</p>
+  <p>$10.00</p>
+  <img src="https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="Eloquent JavaScript cover"/>
+  <button>Delete</button>
+</li>
+*/
 
 /***
 function: create HTML structure for single book and append to page
@@ -20,5 +59,133 @@ input: a book object
 output: none
 ***/
 
+let book = bookStore.inventory[1]
+
+const li = document.createElement('li')
+li.classList.add('card') //add a class of card to li 
+
+let h3 = document.createElement('h3')
+h3.textContent = book.title
+
+let pAuthor = document.createElement('p')
+pAuthor.textContent = book.author
+
+let pPrice = document.createElement('p')
+pPrice.textContent = formatPrice(book.price)
+
+let img = document.createElement('img')
+img.src = book.imageUrl
+
+let button = document.createElement('button')
+button.textContent = "delete"
+
+let bookList = document.querySelector('#book-list')
+bookList.appendChild(li)
+li.appendChild(h3)
+li.appendChild(pAuthor)
+li.appendChild(pPrice)
+li.appendChild(img)
+li.appendChild(button)
+
+
+
 //✅ 7. iterate over all the books in data and show book on page
 
+//Outside of the fuction to delete
+const handleDelete = (e) => {
+  e.target.parentElement.remove() // handleDelete gets us the target which is the button, then we get it's parents 
+}
+
+function renderBook(book) {
+  const li = document.createElement('li')
+  li.classList.add('card') //add a class of card to li 
+
+  let h3 = document.createElement('h3')
+  h3.textContent = book.title
+
+  let pAuthor = document.createElement('p')
+  pAuthor.textContent = book.author
+
+  let pPrice = document.createElement('p')
+  pPrice.textContent = formatPrice(book.price)
+
+  let img = document.createElement('img')
+  img.src = book.imageUrl
+
+  let deleteBtn = document.createElement('button')
+  deleteBtn.textContent = "delete"
+
+
+  // Inside the functinon
+  // const handleDelete = (e) => {
+  //   li.remove()
+  // }
+
+  deleteBtn.addEventListener('click', handleDelete)
+
+
+
+  let bookList = document.querySelector('#book-list')
+  bookList.appendChild(li)
+  li.appendChild(h3)
+  li.appendChild(pAuthor)
+  li.appendChild(pPrice)
+  li.appendChild(img)
+  li.appendChild(deleteBtn)
+
+
+
+
+};
+
+
+let bookArray = bookStore.inventory
+bookArray.forEach(renderBook) // Takes all of the values in bookArray and uses it as the param for renderBook
+
+
+
+//.map 
+let arr = [1, 2, 3, 4, 5]
+let tens = arr.map((el) => { return el * 10 })
+//console.log(tens)
+
+let arrTwo = ['billy', 'bobby', 'brown']
+let his = arrTwo.map((el) => { return 'Hi ' + el })
+//console.log(his)
+
+
+// Saving a funtion and calling it later in map or forEach could be on the code challenge!!!
+let objects = [
+  { name: "dutch", pet: "dog" },
+  { name: "finn", pet: "lab" },
+  { name: "bodhi", pet: "dog" },
+  { name: "moon", pet: "cat" },
+]
+
+//long way of anonymous function created in .map 
+let petsLong = objects.map((el) => {
+  return el.name + " the " + el.pet
+
+})
+console.log(petsLong)
+
+//Name a function and calling it using .map
+const petsHelper = (el) => {
+  return el.name + " the " + el.pet
+}
+let pets = objects.map(petsHelper) // Take the values in "object" and passes them into the petsHelper function one by one 
+console.log(pets)
+
+
+//Getting an individual key and value
+let finn = petsHelper(objects[1])
+console.log(finn)
+
+
+
+//.forEach
+const a = ['billy', 'bobby', 'brown'];
+a.forEach((element) => {
+  //console.log(element)
+  return (element);
+});
